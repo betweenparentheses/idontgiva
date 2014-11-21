2aea77f448e10df59768da035f140c55bce8a1df
Michael Alexander <alexandermw@gmail.com>
2014-11-21 11:26:24 -0800

    Update fuck.js

--- fuck.js
+++ fuck.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
   
   $('#ha').hover(function(e){
-    $(e.target).css({'top' : Math.floor(Math.random() * 500) + 'px', 'left' :  Math.floor(Math.random() * 900) + 'px'});
+    //$(e.target).css({'top' : Math.floor(Math.random() * 500) + 'px', 'left' :  Math.floor(Math.random() * 900) + 'px'});
+  	$(e.target).text('Nope.');
+        $(e.target).css({'top' : Math.floor(Math.random() * 500) + 'px', 
+                     'left' :  Math.floor(Math.random() * 900) + 'px', 
+    	             'background-color' : "orange"
+    });
 	});
   
   $('#ha').click(function(e){
