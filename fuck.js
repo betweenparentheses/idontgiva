dde71d47e132f23ac86ad91aa0712b54017d9397
Michael Alexander <alexandermw@gmail.com>
2014-11-21 11:05:22 -0800

    Create fuck.js

---
+++ fuck.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+  $('#ha').hover(function(e){
+    $(e.target).css({'top' : Math.floor(Math.random() * 500) + 'px', 'left' :  Math.floor(Math.random() * 900) + 'px'});
+	});
+  
+  $('#ha').click(function(e){
+    e.preventDefault();
+  });
+  
+  
+  var count = 10
+  var countDown = setInterval(function(){
+    $('span').text(count);
+    count--;
+    if(count === 0) {
+      $('span').text('');
+      clearInterval(countDown);
+      $('.container').children().remove();
+      $('.container').append('<h1>Total Fucks Given: <strong>0</strong></h1>')
+    };
+  }, 1000);
+  
+});
+
